a00ba45df7427a20ce29909a5e6db9592c50d2d6
hudsonrw7 <hudsonrw7@gmail.com>
2026-06-24 23:59:00 -0400

    proper telnet negotiation with VT100 terminal type and NAWS

--- server.js
+++ server.js
@@ -5,24 +5,56 @@ const PORT = process.env.PORT || 8080
 const wss = new WebSocketServer({ port: PORT, perMessageDeflate: false })
 console.log(`Server running on port ${PORT}`)
 
-function stripTelnet(buf) {
+// Telnet constants
+const IAC  = 0xFF
+const WILL = 0xFB
+const WONT = 0xFC
+const DO   = 0xFD
+const DONT = 0xFE
+const SB   = 0xFA
+const SE   = 0xF0
+const ECHO        = 0x01
+const SGA         = 0x03 // suppress go ahead
+const TERM_TYPE   = 0x18
+const NAWS        = 0x1F
+const NEW_ENVIRON = 0x27
+
+function processTelnet(buf, socket) {
   const out = []
   let i = 0
   while (i < buf.length) {
-    if (buf[i] === 0xFF) {
-      const cmd = buf[i + 1]
-      if (cmd === 0xFB || cmd === 0xFC || cmd === 0xFD || cmd === 0xFE) {
-        i += 3
-      } else if (cmd === 0xFA) {
-        i += 2
-        while (i < buf.length - 1 && !(buf[i] === 0xFF && buf[i + 1] === 0xF0)) i++
-        i += 2
+    if (buf[i] !== IAC) { out.push(buf[i++]); continue }
+    const cmd = buf[i + 1]
+    if (cmd === SB) {
+      // skip subnegotiation until IAC SE
+      i += 2
+      while (i < buf.length - 1 && !(buf[i] === IAC && buf[i + 1] === SE)) i++
+      i += 2
+    } else if (cmd === WILL) {
+      const opt = buf[i + 2]
+      if (opt === ECHO || opt === SGA) {
+        socket.write(Buffer.from([IAC, DO, opt]))   // agree to server echo/sga
       } else {
-        i += 2
+        socket.write(Buffer.from([IAC, DONT, opt])) // refuse others
       }
+      i += 3
+    } else if (cmd === DO) {
+      const opt = buf[i + 2]
+      if (opt === NAWS) {
+        // send window size: 80x24
+        socket.write(Buffer.from([IAC, WILL, NAWS]))
+        socket.write(Buffer.from([IAC, SB, NAWS, 0, 80, 0, 24, IAC, SE]))
+      } else if (opt === TERM_TYPE) {
+        socket.write(Buffer.from([IAC, WILL, TERM_TYPE]))
+        socket.write(Buffer.from([IAC, SB, TERM_TYPE, 0, ...Buffer.from('VT100'), IAC, SE]))
+      } else {
+        socket.write(Buffer.from([IAC, WONT, opt]))
+      }
+      i += 3
+    } else if (cmd === WONT || cmd === DONT) {
+      i += 3
     } else {
-      out.push(buf[i])
-      i++
+      i += 2
     }
   }
   return Buffer.from(out).toString('utf8')
@@ -44,17 +76,12 @@ function connectTelehack() {
   socket.on('connect', () => {
     console.log('Connected to Telehack')
     socket.setNoDelay(true)
-    broadcast('\r\n[Connected to Telehack]\r\n')
   })
 
   socket.on('data', data => {
-    const text = stripTelnet(data)
+    const text = processTelnet(data, socket)
     if (text.length > 0) {
-      let sent = 0
-      for (const ws of clients) {
-        if (ws.readyState === 1) { ws.send(text); sent++ }
-      }
-      console.log(`Broadcast to ${sent}/${clients.size} clients: ${JSON.stringify(text.slice(0, 40))}`)
+      broadcast(text)
     }
   })
 
@@ -64,14 +91,13 @@ function connectTelehack() {
 
   socket.on('close', () => {
     console.log('Telehack connection closed, reconnecting in 3s...')
-    broadcast('\r\n[Disconnected from Telehack - reconnecting...]\r\n')
+    broadcast('\r\n[Disconnected - reconnecting...]\r\n')
     setTimeout(connectTelehack, 3000)
   })
 }
 
 connectTelehack()
 
-// flush Railway's proxy buffer every second
 setInterval(() => {
   for (const ws of clients) {
     if (ws.readyState === 1) ws.send('\x00PING')
@@ -90,7 +116,7 @@ wss.on('connection', ws => {
         if (client !== ws && client.readyState === 1) client.send(text)
       }
     } else if (socket && !socket.destroyed) {
-      socket.write(text + '\r')
+      socket.write(text + '\r\n')
     }
   })
 
